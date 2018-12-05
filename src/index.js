4e7d7a4c588495619dcfc5b152af63017a4c7305
#云淡然 <cloudcome@163.com>
2018-12-05 14:44:19 +0800

    + coolie.render.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * cooolie.render.js
+ * coolie.render.js
  * @author ydr.me
  * @create 2018-12-04 19:41:41
  * @update 2018年12月05日11:47:30
@@ -10,14 +10,73 @@
 
 
 var object = require('blear.utils.object');
+var uglify = require('uglify-js');
 
 var defaults = {
+    /**
+     * Compress options
+     * global_defs
+     * @type Object
+     */
+    globalDefs: {},
 
+    /**
+     * Output options
+     * 是否美化
+     * @type Boolean
+     */
+    beautify: false,
+
+    /**
+     * Output options
+     * 保留注释风格
+     * - true: 同 all
+     * - false: 不保留注释
+     * - regexp: 匹配保留规则
+     * - function: 规则函数，返回 true 保留
+     * @type Boolean | RegExp | Function
+     */
+    keepComments: false,
+
+    /**
+     * 是否支持 IE8
+     * @type Boolean
+     */
+    ie8: false,
+
+    /**
+     * 是否支持资源地图
+     * @type Boolean
+     */
+    sourceMap: false,
+
+    /**
+     * 自定义配置，高于上述配置
+     * @type Null | Object
+     */
+    uglifyOptions: null
 };
 
 
-module.exports = function (js, options) {
+/**
+ * coolie.render.js
+ * @param ast
+ * @param options
+ * @returns {{code: String, map: Object}}
+ */
+module.exports = function (ast, options) {
     options = object.assign({}, defaults, options);
-
-
+    var uglifyOptions = {
+        compress: {
+            global_defs: options.globalDefs
+        },
+        output: {
+            beautify: options.beautify,
+            comments: options.keepComments
+        },
+        ie8: options.ie8,
+        sourceMap: options.sourceMap
+    };
+    object.assign(true, uglifyOptions, options.uglifyOptions);
+    return uglify.minify(ast, uglifyOptions);
 };
--- test/test.index.js
+++ test/test.index.js
@@ -8,13 +8,136 @@
 'use strict';
 
 var expect = require('chai-jasmine').expect;
-var parser = require('../src/index.js');
+var render = require('../src/index.js');
 var fs = require('fs');
 var path = require('path');
 var uglify = require('uglify-js');
 
 describe('coolie.render.js', function () {
+    it('globalDefs', function () {
+        var ast = uglify.parse([
+            'if(A) {',
+            '    alert(1);',
+            '} else {',
+            '    alert(2)',
+            '}'
+        ].join('\n'));
+        var ret = render(ast, {
+            globalDefs: {
+                A: false,
+                '@alert': 'console.log'
+            }
+        });
+        expect(ret.code).toEqual('console.log(2);');
+    });
 
+    it('beautify', function () {
+        var ast = uglify.parse([
+            'alert(a + "b");'
+        ].join('\n'));
+        var ret = render(ast, {
+            beautify: false
+        });
+        expect(ret.code).toEqual('alert(a+"b");');
+
+        ret = render(ast, {
+            beautify: true
+        });
+        expect(ret.code).toEqual('alert(a + "b");');
+    });
+
+    it('keepComments', function () {
+        var ast = uglify.parse([
+            '/**',
+            ' * a',
+            ' */',
+            '',
+            '/* b */',
+            '',
+            '// c',
+            ''
+        ].join('\n'));
+        var ret = render(ast, {
+            keepComments: false
+        });
+        expect(ret.code).toEqual('');
+
+        ret = render(ast, {
+            keepComments: true
+        });
+        expect(ret.code).toMatch(/^\s\*\sa$/m);
+        expect(ret.code).toMatch(/^\/\* b \*\/$/m);
+        expect(ret.code).toMatch(/^\/\/ c$/m);
+
+        ret = render(ast, {
+            keepComments: /^\sb\s$/
+        });
+        expect(ret.code).toEqual('/* b */\n');
+
+        ret = render(ast, {
+            keepComments: function (node, token) {
+                if (token.value === ' b ') {
+                    return true;
+                }
+
+                return false;
+            }
+        });
+        expect(ret.code).toEqual('/* b */\n');
+    });
+
+    it('ie8', function () {
+        var ast = uglify.parse([
+            'var o = {',
+            'for: "bar",',
+            '};',
+            ''
+        ].join('\n'));
+        var ret = render(ast, {
+            ie8: false
+        });
+        expect(ret.code).toEqual('var o={for:"bar"};');
+
+        ret = render(ast, {
+            ie8: true
+        });
+        expect(ret.code).toEqual('var o={"for":"bar"};');
+    });
+
+    it('sourceMap', function () {
+        var ast = uglify.parse([
+            'var o = {',
+            'for: "bar",',
+            '};',
+            ''
+        ].join('\n'));
+        var ret = render(ast, {
+            sourceMap: false
+        });
+        expect(ret.map).toEqual(undefined);
+
+        ret = render(ast, {
+            sourceMap: true
+        });
+        expect(ret.map).toEqual('{"version":3,"sources":[],"names":[],"mappings":""}');
+    });
+
+    it('uglifyOptions', function () {
+        var ast = uglify.parse([
+            'function abc(def, ghi) {',
+            '   console.log(def + ghi);',
+            '}',
+            ''
+        ].join('\n'));
+        var ret = render(ast, {
+            uglifyOptions: {
+                mangle: {
+                    reserved: ['def']
+                }
+            }
+        });
+        expect(ret.code).toEqual('function abc(def,o){console.log(def+o)}');
+    });
 });
 
 
